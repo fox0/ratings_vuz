ec9ee44df6a09d4f2b1a62c1457435597c92ded4
fox0 <red-fox0@yandex.ru>
2019-02-12 09:33:10 +0700

    +1

--- tatings_vuz.py
+++ tatings_vuz.py
@@ -6,10 +6,14 @@ from bs4 import BeautifulSoup
 from settings import RATINGS
 
 
-def parse9(url):
+def parse1(soup: BeautifulSoup):
+    # https://www.topuniversities.com/universities/0000
+    return soup.find('div', {'class': 'rank-r'}).text
+
+
+def parse9(soup: BeautifulSoup):
     # https://www.4icu.org/reviews/rankings/university-ranking-0000.htm
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    # todo rewrite
     result = []
     for i in soup.find_all('tr'):
         r = i.text.strip().replace('\n', ' ')
@@ -20,11 +24,14 @@ def parse9(url):
 
 def main():
     parsers = {
+        1: parse1,
         9: parse9,
     }
     for i, url in RATINGS.items():
-        text = parsers[i](url)
-        print(text)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        text = parsers[i](soup)
+        print('{}. {}'.format(i, text))
 
 
 if __name__ == '__main__':
