aa222bf70c9692c49252a176c7955abc31502df3
fox0 <red-fox0@yandex.ru>
2019-02-12 09:53:10 +0700

    +2

--- settings.example.py
+++
@@ -1,3 +0,0 @@
-RATINGS = {
-    9: 'https://www.4icu.org/reviews/rankings/university-ranking-0000.htm',
-}
--- tatings_vuz.py
+++ tatings_vuz.py
@@ -1,4 +1,5 @@
-import re
+from pprint import pprint
+from json import JSONEncoder, JSONDecoder
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,6 +10,12 @@ from settings import RATINGS
 def parse1(soup: BeautifulSoup):
     # https://www.topuniversities.com/universities/0000
     return soup.find('div', {'class': 'rank-r'}).text
+
+
+def parse2(soup: BeautifulSoup):
+    # http://www.webometrics.info/en/detalles/000
+    cols = soup.find('table', {'id': 'mytable'}).find_all('tr')[1].find_all('td')
+    return 'Country Rank %s. World Ranking %s' % (cols[2].text, cols[0].text)
 
 
 def parse9(soup: BeautifulSoup):
@@ -23,15 +30,26 @@ def parse9(soup: BeautifulSoup):
 
 
 def main():
-    parsers = {
-        1: parse1,
-        9: parse9,
-    }
+    result = {}
     for i, url in RATINGS.items():
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
-        text = parsers[i](soup)
-        print('{}. {}'.format(i, text))
+        result[i] = globals()['parse%d' % i](soup)
+
+    with open('db.json') as f:
+        old_result = {}
+        for k, v in JSONDecoder().decode(f.read()).items():
+            old_result[int(k)] = v
+
+    diff = []
+    for i in result.keys():
+        if result[i] != old_result[i]:
+            diff.append('%s' % RATINGS[i])
+            diff.append('old: %s' % old_result[i])
+            diff.append('new: %s' % result[i])
+            diff.append('')
+    print('\n'.join(diff))
+    # todo save
 
 
 if __name__ == '__main__':
